09e3b964f84bbcc33b7af8c15ab3adc7eedde929
Jibon Bikash Roy <jibon.bikash@gmail.com>
2015-10-29 19:14:39 +0600

    view file for communication and classadd

---
+++ application/views/default/home/communication.php
@@ -0,0 +1,172 @@
+<?php
+$CI =& get_instance();
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/templates/'.$CI->get_template(); ?>/css/bootstrap-wysihtml5.css"></link>
+<script src="<?php echo base_url().'assets/templates/'.$CI->get_template(); ?>/js/wysihtml5-0.3.0.js"></script> 
+<script src="<?php echo base_url().'assets/templates/'.$CI->get_template(); ?>/js/bootstrap-wysihtml5.js"></script> 
+<div id="system_content" class="system_content_margin">
+    <div class="division">
+        <h3>সংশ্লিষ্ট কর্মকর্তা - বিভাগ</h3>
+    <?php
+ //   print_r($divisions);
+    if(isset($divisions)):
+        foreach ($divisions as $divisions):
+      //  echo $divisions['divname'];
+   //  print_r($divisions);
+ echo '<input name="division[]" type="checkbox" value="'.$divisions['divid'].'" /><label for='.$divisions['divname'].'>'.$divisions['divname'].'</label>';
+        endforeach;
+        
+    endif;
+    ?>
+    </div>
+     <div class="zilla">
+        <h3>সংশ্লিষ্ট কর্মকর্তা - জেলা</h3>
+    <?php
+    if(isset($zillas)):
+        foreach ($zillas as $zillas):
+ echo '<input name="zilla[]" type="checkbox" value="'.$zillas['zillaid'].'" /><label for='.$zillas['zillaname'].'>'.$zillas['zillaname'].'</label>';
+        endforeach;
+        
+    endif;
+    ?>
+    </div>
+    <div class="zilla">
+         <h3>সংশ্লিষ্ট কর্মকর্তা - উপজেলা</h3>
+       <div class="col-md-3">
+    <select name="zilaname" class="form-control" id="zila_id">
+      <?php
+      $CI->load_view('dropdown',array('drop_down_options'=>$zillasdp,'drop_down_selected'=>''));
+      ?>
+    </select>
+     </div>
+         
+         <div class="col-md-9">
+             <div id="upozilaname"></div> 
+             
+         </div>
+    </div>
+    <div class="clearfix"></div>
+    
+     <div class="perticularinstitute">
+         <h3>প্রতিষ্ঠান সমূহ</h3>
+     <div class="col-md-2">
+    <select name="zilaname" class="form-control" id="zillaid">
+      <?php
+      $CI->load_view('dropdown',array('drop_down_options'=>$zillasdp,'drop_down_selected'=>''));
+      ?>
+    </select>
+     </div>
+         <div class="col-md-2">
+   <select name="upozilla" id="upozilla_id" class="selectbox-1 zilla validate[required]">
+          <option value=""><?php echo $this->lang->line('SELECT');?></option>
+    </select>      
+     </div>
+         <div class="col-md-8">
+             <div id="schools" class="schools"></div>     
+             
+         </div>    
+         
+     </div>     
+    <div class="clearfix"></div>
+        <textarea class="textarea" placeholder="Enter text ..." style="width: 810px; height: 200px"></textarea>
+    
+</div>
+<style>
+    .schools label, .zilla label, .division label{ display: inline !important}
+    .schools{ margin-left: 10px;}
+</style>
+
+<script>
+    $(document).on("change","#zila_id",function()
+        {
+            var zilla_id=$(this).val();
+
+            if(zilla_id>0)
+            {
+                $.ajax({
+                    url: '<?php echo $CI->get_encoded_url('home/getUpazilacheckbox'); ?>',
+                    type: 'POST',
+                    dataType: "JSON",
+                    data:{zilla_id:zilla_id},
+                    success: function (data, status)
+                    {
+                    $('#upozilaname').html(data); 
+                    },
+                    error: function (xhr, desc, err)
+                    {
+                        console.log("error");
+
+                    }
+                });
+            }
+            else
+            {
+                $("#upozilaname").val('');
+            }
+        });
+   
+   
+   $(document).on("change","#zillaid",function()
+        {
+            var zilla_id=$(this).val();
+
+            if(zilla_id>0)
+            {
+                $.ajax({
+                    url: '<?php echo $CI->get_encoded_url('home/getUpazila'); ?>',
+                    type: 'POST',
+                    dataType: "JSON",
+                    data:{zilla_id:zilla_id,},
+                    success: function (data, status)
+                    {
+
+                    },
+                    error: function (xhr, desc, err)
+                    {
+                        console.log("error");
+
+                    }
+                });
+            }
+            else
+            {
+                $("#upozilla_id").val('');
+            }
+        });
+  
+   $(document).on("change","#upozilla_id",function()
+        {
+            var upozilla_id=$(this).val();
+            var zillaid = $("#zillaid").val();
+            if(upozilla_id>0)
+            {
+                $.ajax({
+                    url: '<?php echo $CI->get_encoded_url('home/getUpazilaschoolcheckbox'); ?>',
+                    type: 'POST',
+                    dataType: "JSON",
+                    data:{zillaid:zillaid, upozilla_id: upozilla_id},
+                    success: function (data, status)
+                    {
+                    $('#schools').html(data); 
+                    },
+                    error: function (xhr, desc, err)
+                    {
+                        console.log("error");
+
+                    }
+                });
+            }
+            else
+            {
+                $("#upozilaname").val('');
+            }
+        });
+     $('.textarea').wysihtml5();
+ </script>
--- application/views/default/institute/classadd.php
+++ application/views/default/institute/classadd.php
@@ -17,20 +17,40 @@ $CI=& get_instance();
 <div id="system_content" class="system_content_margin">
     <div id="system_action_button_container" class="system_action_button_container">
         <?php
+        
+//       $data = unserialize('a:2:{i:2;a:2:{i:0;i:10;i:1;i:11;}i:8;a:8:{i:0;i:10;i:1;i:11;i:2;i:110;i:3;i:111;i:4;i:112;i:5;i:113;i:6;i:114;i:7;i:115;}}');
+//       print_r($data);
+//       foreach($data as $key => $value){
+//          foreach($value as $k => $v){
+//              echo $v.'-'; 
+//          }
+//       }
+       
+//         
      //   $CI->load_view('system_action_buttons');
+/*        
+$temp_array = array();
+$temp_array['uid'] = "a new value";
+$temp_array['pid'] = "another new value";
 
-//$data = unserialize("a:3:{i:1;a:1:{i:0;i:1;}i:2;a:1:{i:0;i:2;}i:3;a:1:{i:0;i:3;}}");
+$temp_array['akey'] = "some value";
+echo $store_database = serialize($temp_array);
+
+$data = unserialize('a:3:{s:3:"uid";s:11:"a new value";s:3:"pid";s:17:"another new value";s:4:"akey";s:10:"some value";}');
+*/
 //
-//echo $itemcount = count($data);
+////echo $itemcount = count($data);
 //
 //
+//print_r($data);
 //foreach($data as $key => $value){
-//            
+//            echo $value;
 //                 foreach($value as $k => $v){
-//                     echo $v;
+//                     echo $v.'-';
+//                     
 //                 }
-//             
-//
+             
+
 //         }
 //         
         ?>
@@ -40,10 +60,15 @@ $CI=& get_instance();
     
     ?>
         
-        <table width="75%" border="0" class="form-table" id="customFields">
-
+        <table width="75%" border="0" class="table" id="customFields">
+<tr>
+    <th  width="25%">স্তর </th>
+    <th  width="25%">শ্রেণী </th>
+    <th  width="25%">তারিখ </th>
+  </tr>
   <tr>
-    <td><select class="selectbox-1" name="education_level" id="education_level">
+    <td>
+        <select class="selectbox-1" name="education_level[]" id="education_level">
 	<option value="">বাছাই করুন ...</option>
         <?php
         if($institute['is_primary']==1){
@@ -57,14 +82,15 @@ $CI=& get_instance();
             echo '<option value="7">উচ্চ মাধ্যমিক/ আলীম</option>';
         }
         ?>
-</select></td>
-    <td> <select name="classesid" id="classes" class="selectbox-1 zilla validate[required]">
+        </select>
+    </td>
+    <td> <select name="classesid[]" id="classes" class="selectbox-1 zilla validate[required]">
          <option value=""><?php echo $this->lang->line('SELECT');?></option>
          </select></td>
-         <td><input type="text" name="cladddate" class="datepicker" value="<?php echo date("Y-m-d"); ?>"/><a href="javascript:void(0);" class="addCF">Add</a></td>
+         <td><input type="text" name="cladddate[]" class="datepicker" value="<?php echo date("Y-m-d"); ?>"/>&nbsp;&nbsp;<a href="javascript:void(0);" class="addCF external btn btn-primary">Add More</a></td>
   </tr>
   <tr>
-    <td colspan="3"><div id="classesname"></div></td>
+    <td colspan="3"><div class="classesname" id="classesname"></div></td>
   </tr>
 </table>
     
@@ -100,7 +126,7 @@ $CI=& get_instance();
       echo '<input type="hidden" name="is_secondary" value="0"/>';  
     }
     ?>
-    <input type="submit" name="classsave" value="submit">
+    <input type="submit" name="classsave" value="submit" class="btn btn-default">
 </form>
      <?php //echo 'education_type_ids'.$institute['education_type_ids']?>
      <?php //echo 'is_primary'.$institute['is_primary']?>
@@ -166,20 +192,12 @@ $CI=& get_instance();
 
 </script>
 <script>
-    $(document).ready(function() {
 
-    // assuming the controls you want to attach the plugin to 
-    // have the "datepicker" class set
-    $('input.datepicker').Zebra_DatePicker();
-    
-
- });
- 
  $(document).ready(function(){
      var num = 1;
 $(".addCF").click(function(){
     
-    $("#customFields").append('<tr valign="top"><td><select class="selectbox-1" name="education_level" id="education_level'+ num +'" onchange="getclasselist('+ num +');"><option value="">বাছাই করুন ...</option> <?php
+    $("#customFields").append('<tr valign="top"><td><select class="selectbox-1" name="education_level[]" id="education_level'+ num +'" onchange="getclasselist('+ num +');"><option value="">বাছাই করুন ...</option> <?php
         if($institute['is_primary']==1){
             echo '<option value="5">প্রাথমিক/ ইবতেদায়ী</option>';
         }
@@ -190,12 +208,16 @@ $(".addCF").click(function(){
          if($institute['is_higher']==1){
             echo '<option value="7">উচ্চ মাধ্যমিক/ আলীম</option>';
         }
-        ?></select></td><td><select onchange="getclassesubject('+ num +');" name="classesid" id="classesid'+ num +'" class="selectbox-1 zilla validate[required]"> <option value=""><?php echo $this->lang->line('SELECT');?></option></select></td><td><input type="text" name="cladddate" class="datepicker" value="<?php echo date("Y-m-d"); ?>"/>&nbsp;&nbsp;<a href="javascript:void(0);" class="remCF">Remove</a></td></tr><tr><td colspan="3"><div id="classesname'+ num +'"></div></td></tr>');
-    num = num + 1;               
+        ?></select></td><td><select onchange="getclassesubject('+ num +');" name="classesid[]" id="classesid'+ num +'" class="selectbox-1 zilla validate[required]"> <option value=""><?php echo $this->lang->line('SELECT');?></option></select></td><td><input type="text" name="cladddate[]" class="datepicker" value="<?php echo date("Y-m-d"); ?>"/>&nbsp;&nbsp;<a href="javascript:void(0);" class="remCF external btn btn-danger">Remove</a></td></tr><tr><td colspan="3"><div class="classesname" id="classesname'+ num +'"></div></td></tr>');
+    num = num + 1;     
+    $('.datepicker').Zebra_DatePicker();
 	});
+        
     $("#customFields").on('click','.remCF',function(){
         $(this).parent().parent().remove();
     });
+    
+      $('.datepicker').Zebra_DatePicker();
 });
 
  function getclasselist(num) {
@@ -260,3 +282,8 @@ $(".addCF").click(function(){
             
     }
  </script>
+ <style>
+    .classesname label {
+    display: inline !important;
+}
+ </style>
